7be070afd8a9fd6a08bde9fa94a5597bee9df5bd
Chirag jethva <chiragjethva@Chirags-MacBook-Pro-2.local>
2021-05-07 11:07:54 +0530

    v12

--- script.sh
+++ script.sh
@@ -8,6 +8,18 @@ sudo chmod +x /usr/bin/docker-compose
 git clone https://github.com/chiragjethva/devops-example-25981.git
 cd devops-example-25981
 
+mv .env.example ./src/.env
+perl -i -pe's/postgres_pwd=.*/postgres_pwd='"${postgres_pwd}"'/g' .env
+perl -i -pe's/DATABASE_URL=.*/DATABASE_URL='"${DATABASE_URL}"'/g' .env
+perl -i -pe's/REDIS_URL=.*/REDIS_URL='"${REDIS_URL}"'/g' .env
+
+cd ./src/
+
+docker-compose -f docker-compose.override.yml up -d
+docker-compose exec web python3 manage.py makemigrations
+docker-compose exec web python3 manage.py migrate
+
+
 export DEBUG=1
 export HOST=localhost
 export PORT=8000
